963e64d60a3417ca7e5febdf2d912601a1468ebe
Gustavo Bonifacio <g.bonifacio@aluno.ifsp.edu.br>
2026-06-07 17:07:34 -0300

    feat: missao 2 - work loop e fiber tree

--- didact.js
+++ didact.js
@@ -20,18 +20,51 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type)
-
-  Object.keys(element.props)
-    .filter(key => key !== "children")
-    .forEach(name => dom[name] = element.props[name])
-
-  element.props.children.forEach(child => render(child, dom))
-  container.appendChild(dom)
+      : document.createElement(fiber.type)
+  updateDom(dom, {}, fiber.props)
+  return dom
 }
 
-const Didact = { createElement, render }
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  const isFunctionComponent = fiber.type instanceof Function
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
+  }
+
+  if (fiber.child) return fiber.child
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
+  }
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) fiber.dom = createDom(fiber)
+  reconcileChildren(fiber, fiber.props.children)
+}
+
+const Didact = { createElement }
